d72420b172b012ea24c2b8d840182499f44d7a04
Andrew Raffensperger <raffy@me.com>
2024-03-19 16:37:12 -0700

    add crappy proof gen

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # XCTENS.sol
 
-1. `foundup`
+1. [`foundryup`](https://book.getfoundry.sh/getting-started/installation)
 1. `npm i`
 1. `npm run test`
---
+++ test/make-proof.js
@@ -0,0 +1,15 @@
+import {ethers} from 'ethers';
+
+// same demo key as TOR
+const signingKey = new ethers.SigningKey('0xbd1e630bd00f12f0810083ea3bd2be936ead3b2fa84d1bd6690c77da043e9e02'); 
+const signer = ethers.computeAddress(signingKey);
+
+function whitelist(label, address) {
+	label = ethers.ensNormalize(label);
+	if ([...label].length < 4) throw new Error('too short');
+	let hash = ethers.solidityPackedKeccak256(['address', 'address', 'string'], [signer, address, label]);
+	let proof = signingKey.sign(hash).serialized;
+	return {label, proof, address};
+}
+
+console.log(whitelist('raffy', '0x51050ec063d393217B436747617aD1C2285Aeeee'));
